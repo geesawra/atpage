745755478747059fdc95001a38833fa38190899e
geesawra <hello@geesawra.industries>
2024-11-12 18:29:40 +0100

    build.sh: remove mentions of atproto

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@ set -e
 set -x
 rm -rf pkg
 wasm-pack build --release --no-typescript --target web atresolver
-# wasm-pack build --release --no-typescript --target web atproto
 mkdir pkg
-cp atproto/pkg/* pkg
 cp atresolver/pkg/* pkg
